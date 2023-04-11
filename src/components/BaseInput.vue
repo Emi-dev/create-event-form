9f79eab8a8c1daf7700ae41c8cb2e56dad7a57d2
emiclar <emiclar@aflac.com>
2023-04-11 09:54:20 -0400

    First commit

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,19 @@
+<!-- eslint-disable prettier/prettier -->
+<script setup>
+  const props = defineProps({
+    label: {
+      type: String,
+      default: ''
+    }
+  })
+</script>
+
+<template>
+  <label>{{ label }}</label>
+  <input 
+    :placeholder="label"
+    class="field"
+  />
+</template>
+
+<style></style>
